193834902a863c293fc3a0667ca57e6e5b2a159f
Keita <gk19901020@gmail.com>
2017-01-15 10:59:50 +0900

    プロジェクト ファイルを追加します。

---
+++ BotTriggerTest/OAuth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotTriggerTest
+{
+    class OAuth
+    {
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
+        public int ext_expires_in { get; set; }
+        public string access_token { get; set; }
+    }
+}
---
+++ BotTriggerTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Microsoft.Bot.Connector;
+using System.Web;
+
+namespace BotTriggerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // POSTする対象のURL
+            var token = "xoxp-75124025591-75124025671-127709936900-ed820c6b394ae43216e1a3ef67cf63d0"; // トークンの取得（http://qiita.com/rubytomato@github/items/6558bfdb37d982891c09）
+            var channel = "@keitabot"; // 投稿するチャンネル(#は不要）。Direct Messageの場合は@username
+                var text = "Hello World"; // 投稿するメッセージ
+                var url = "https://slack.com/api/chat.postMessage?token=" + token + "&channel=" + channel + "&text=" + text + "&as_user=true";
+
+                // リクエスト作成
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.Method = "GET";
+
+                // レスポンスの取得
+                WebResponse res = req.GetResponse();
+
+                // 結果の読み込み
+                string htmlStrings = "";
+                using (Stream resStream = res.GetResponseStream())
+                using (var reader = new StreamReader(resStream, Encoding.GetEncoding("Shift_JIS")))
+                    htmlStrings = reader.ReadToEnd();
+                Console.WriteLine(htmlStrings);
+            Console.ReadLine();
+
+        }
+    }
+}
